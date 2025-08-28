37689e375a77ed80b7585ca2eb983606c290aee7
Amit_DevOps_Eng <101174985+Amit-SourceSoft@users.noreply.github.com>
2025-08-28 11:52:28 +0000

    Vairables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "amit@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+
+// accountId = 2 --> Not allowed
+
+accountEmail = "hc@gmail.com"
+accountPassword="1242566"
+accountCity = "mathura"
+console.log(accountEmail)
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
