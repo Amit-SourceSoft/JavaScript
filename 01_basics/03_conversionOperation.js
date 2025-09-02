ff86b7b6c9458ada755179a6833654607c72d7fd
Amit_DevOps_Eng <101174985+Amit-SourceSoft@users.noreply.github.com>
2025-09-02 05:50:48 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -14,7 +14,7 @@
 
 let isLoggedIn = 7
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false
@@ -22,6 +22,27 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+// console.log(typeof stringNumber)
 
-console.log("3" + "6")
+// console.log("3" + "6")
+// console.log(1 +"2"+2)
+
+// **********Operations **************
+
+let value =3
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "Amit "
+let str2 = "Kumar"
+let str3 = str1 + str2
+console.log(str3)
+
+// console.log(+true)
+// console.log(+"")
+let num1, num2, num3
+num1=num2=num3 = 2+2
+
+let gameCounter =100;
+++gameCounter;
+console.log(gameCounter)
