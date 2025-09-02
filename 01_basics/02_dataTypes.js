b31831aab3a837ae84c1c7edb49ee9d1d11c61d9
Amit_DevOps_Eng <101174985+Amit-SourceSoft@users.noreply.github.com>
2025-09-02 04:45:58 +0000

    Data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict" // treat all js code as newer version
+//alert("Hello") // we are using node js not browser
+ let name = "amit"
+ let age = 18
+ let isLoggedIn = false
+ 
+ //null => standalone value
+ //undefined =>
+// synbol => unique
+
+ // object
+
+ console.log(typeof null) // object
+ console.log(typeof undefined) //undefined
