c4ea995cc6594b489acd08b89d9c907cca7d1a2b
Amit_DevOps_Eng <101174985+Amit-SourceSoft@users.noreply.github.com>
2025-09-16 04:59:12 +0000

    Object part3

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,30 @@
+const course = {
+    coursename:"JS in Hindi",
+    price:"999",
+    courseInstructor:"Hitesh"
+}
+
+//course.courseInstructor
+const {courseInstructor} = course
+console.log(courseInstructor)
+
+//Destructure
+// const navbar = ({company}) => {
+
+// }
+// navbar(company = "hitesh")
+
+
+//JSON
+// {
+//     "name":"Hitesh",
+//     "coursename":"JS in Hindi",
+//     "price":"Free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
